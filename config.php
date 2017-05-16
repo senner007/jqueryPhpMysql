63183126cbe3d601648de4b6778af91c3bcc4e3b
senner007 <nielshtg@hotmail.com>
2017-05-16 19:55:51 +0200

    init commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+########## MySql details (Replace with yours) #############
+$username = "root"; //mysql username
+$password = "root"; //mysql password
+$hostname = "localhost"; //hostname
+$databasename = 'myTest'; //databasename
+
+//connect to database
+$mysqli = new mysqli($hostname, $username, $password, $databasename);
+
+
+?>
